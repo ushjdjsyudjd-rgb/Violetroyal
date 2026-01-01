2f430be6faf1d1b8c3705a9e1e994f51c1fd4109
ushjdjsyudjd-rgb <ushjdjsyudjd@gmail.com>
2026-01-01 19:45:35 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Violetroyal/', // این خط باعث می‌شود تمام فایل‌های CSS و JS در گیت‌هاب پیدا شوند
+  base: '/Violetroyal/', 
 })
